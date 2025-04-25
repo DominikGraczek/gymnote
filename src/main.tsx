b0bbf9082e37b537f2a1fc4d4f3e60f95e266faa
DominikGraczek <dominik.graczek@gmail.com>
2025-04-25 15:15:35 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { UserDataProvider } from "./context/UserContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <UserDataProvider>
-      <App />
-    </UserDataProvider>
-  </StrictMode>
+  <UserDataProvider>
+    <App />
+  </UserDataProvider>
 );
